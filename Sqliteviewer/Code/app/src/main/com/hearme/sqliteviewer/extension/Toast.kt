d7b19346a883efef2a1f3d51858a210aad48c3b0
李绍丰 <47840575+Crazydear@users.noreply.github.com>
2022-08-27 20:15:14 +0800

    Add files via upload

--- Sqliteviewer/Code/app/src/main/com/hearme/sqliteviewer/extension/Toast.kt
+++ Sqliteviewer/Code/app/src/main/com/hearme/sqliteviewer/extension/Toast.kt
@@ -1,1 +1,12 @@
-1
+package com.hearme.sqliteviewer.extension
+
+import android.content.Context
+import android.widget.Toast
+
+fun String.showToast(context: Context, duration: Int = Toast.LENGTH_LONG) {
+    Toast.makeText(context, this, duration).show()
+}
+
+fun Int.showToast(context: Context, duration: Int = Toast.LENGTH_LONG) {
+    Toast.makeText(context, this, duration).show()
+}
